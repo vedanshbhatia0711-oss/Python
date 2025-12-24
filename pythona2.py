44d56b474bbc448767408f8060dfa61f9fe8bee1
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-24 12:25:54 +0800

    Add files via upload

---
+++ pythona2.py
@@ -0,0 +1,10 @@
+#print a sentence
+print("Hello world\n")
+#print a number
+print("6\n")
+#next line after print
+print("hello python\n")
+#pass multiple arguments for printing
+print("hello", 5)
+#end argument
+print("welcome to\n", end="*")
