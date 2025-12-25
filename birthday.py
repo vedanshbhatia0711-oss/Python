31947fd376e3ddd79f67b6ebfc620d46dc89d137
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-25 11:55:38 +0800

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,11 @@
+print("Raju has five friends\n")
+y="13 January 2025"
+print("Arjun\n", y)
+x="14 January 2009"
+print("Ujwal\n", x)
+z="15 June 2010"
+print("Dhanvin\n", z)
+t="16 March 2012"
+print("Rohan\n", t)
+u="12 April 2011"
+print("Shivesh\n", u)
