69bd68405349792a86e1f0d50d77a80680f57db3
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-25 12:30:07 +0800

    Add files via upload

---
+++ pythona5.py
@@ -0,0 +1,26 @@
+Name="penguin\n"
+age=16
+weight=40.5
+is_student=True
+#printing different types of data 
+print("name:", Name)
+print("Data type of name is:\n", type(Name))
+print("Weight:", weight)
+print("Data type of weight is:\n", type(weight))
+print("age:", age)
+print("Data type of age is:\n", type(age))
+print("is_student:", is_student)
+print("Data type of is_student is:\n", type(is_student))
+#Typecasting data types
+weight=int(weight)
+print("Weight:", weight)
+print("Data type of weight is:\n", type(weight))
+age=float(age)
+print("age:", age)
+print("Data type of age is:\n", type(age))
+is_student=str(is_student)
+print("is_student:", is_student)
+print("Data type of is_student is:\n", type(is_student))
+Name=bool(Name)
+print("name:", Name)
+print("Data type of name is:\n", type(Name))
