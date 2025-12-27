ae94f3e3777993ea745ee0b162a7e1e4ebda94bd
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-27 18:17:49 +0800

    Add files via upload

---
+++ pythona10.py
@@ -0,0 +1,8 @@
+Math=int(input("Enter your marks here: "))
+English=int(input("Enter your marks here"))
+Science=int(input("Enter your markks here"))
+Hindi=int(input("Enter your marks here"))
+sum=Math+English+Science+Hindi
+print("Total marks=", sum)
+perc=(sum/400)*100
+print("overall percentage =", perc)
