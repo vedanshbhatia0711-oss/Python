40bf718854cd0fdcfe9202165d8cdafb3246a17e
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-27 17:57:55 +0800

    Add files via upload

---
+++ pythona8.py
@@ -0,0 +1,11 @@
+tree1=50
+tree2=60
+tree3=70
+tree4=80
+tree5=90
+sum=tree1 + tree2 + tree3 + tree4 + tree5
+#finding the sum
+print("sum of trees:\n",sum)
+average=sum/5
+#finding average of trees
+print("average of trees: \n", average)
