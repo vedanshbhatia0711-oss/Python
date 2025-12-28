0074b7e8a8afe310771163791ef7c2c9f3abf974
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-28 14:29:48 +0800

    Add files via upload

---
+++ pythona13.py
@@ -0,0 +1,7 @@
+i=int(input("Enter number here:"))
+if (i < 15):
+    print("i is smaller than 15\n")
+    print("i is in the if block")
+else:
+    print("i is greater than 15")
+    print("i is in the else block")
