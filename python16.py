0a8c3a484f52a1553a370ae69ef409161778d425
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-30 12:24:24 +0800

    Add files via upload

---
+++ python16.py
@@ -0,0 +1,19 @@
+a=10
+b=10
+c=12
+print(a != b)
+print(b != c)
+a="python"
+b="Javascript"
+if a != b:
+    print(a, "and", b, "are different")
+a=4
+b=5
+if (a==1) != (b==5):
+    print("Hello")
+a=int(input("Enter number here:"))
+if a%2 != 0:
+    print("a is an odd number")
+else:
+    print("a is an even number")
+
