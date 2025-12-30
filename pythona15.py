7077ef4656e5bb76528ddb1330e51904d9afb6ca
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-30 12:15:01 +0800

    Add files via upload

---
+++ pythona15.py
@@ -0,0 +1,18 @@
+a=12
+b=10
+c=0
+if a and b and c: 
+    print("All the numbers have boolean value as true\n")
+else:
+    print("At least one number has boolean value as false\n")
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("Either of the number is geater than 0")
+else: 
+    print("no number is greater than 0")
+if b>0 or c>0:
+    print("either of the numbers is greater than 0")
+else:
+    print("None of the numbers is greater than 0")
