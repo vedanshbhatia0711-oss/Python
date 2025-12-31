fc77d051ea91560107865131445feb12e2c32889
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-31 13:10:56 +0800

    Add files via upload

---
+++ alphabetnumber.py
@@ -0,0 +1,12 @@
+c=input("Enter something here:")
+if c > "0" and c <= "9":
+    print("c is a number")
+elif c >= "a" and c <= "z":
+    print("c is an alphabet")
+elif c >= "A" and c <= "z":
+    print(" c is a capital alphabet")
+else:
+    print("c is neither an aphabet nor a number")
+
+
+
