5d9a933552c428d22d9ca5c8d6b9ffa9f0c69f26
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2025-12-31 13:09:23 +0800

    Add files via upload

---
+++ ACSII.py
@@ -0,0 +1,56 @@
+x=int(input("Enter ACSII between sixty five and ninety:"))
+if x==65:
+    print("x stands for A")
+elif x==66:
+    print("x stands for B")
+elif x==67:
+    print("x stands for C")
+elif x==68:
+    print("x stands for D")
+elif x==69:
+    print("x stands for E")
+elif x==70:
+    print("x stands for F")
+elif x==71:
+    print("x stands for G")
+elif x==72:
+    print("x stands for H")
+elif x==73:
+    print("x stands for I")
+elif x==74:
+    print("x stands for J")
+elif x==75:
+    print("x stands for K")
+elif x==76:
+    print("x stands for L")
+elif x==77:
+    print("x stands for M")
+elif x==78:
+    print("x stands for N")
+elif x==79:
+    print("x stands for O")
+elif x==80:
+    print("x stands for P")
+elif x==81:
+    print("x stands for Q")
+elif x==82:
+    print("x stands for R")
+elif x==83:
+    print("x stands for S")
+elif x==84:
+    print("x stands for T")
+elif x==85:
+    print("x stands for U")
+elif x==86:
+    print("x stands for V")
+elif x==87:
+    print("x stands for W")
+elif x==88:
+    print("x stands for X")
+elif x==89:
+    print("x stands for Y")
+elif x==90:
+    print(" x stands for Z")
+else:
+    print("Enter valid input")
+
