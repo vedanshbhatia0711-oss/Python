bc02eb3235d81c1a3ea51557be4a7fd68ac22532
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2026-01-01 12:27:33 +0800

    Add files via upload

---
+++ python23.py
@@ -0,0 +1,10 @@
+print("Enter a number(numerator):")
+numn=int(input())
+print("Enter a number(denominator):")
+numd=int(input())
+if numn%numd==0:
+    print(str(numn)+ " is divisible by "+ str(numd))
+else:
+    print(str(numn)+ " is not divisible by "+ str(numd))
+    
+
