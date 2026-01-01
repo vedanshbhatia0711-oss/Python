69974994828a83dbc5f01f1e13262dde26c179d6
vedanshbhatia0711-oss <vedanshbhatia0711@gmail.com>
2026-01-01 12:16:08 +0800

    Add files via upload

---
+++ python22.py
@@ -0,0 +1,13 @@
+v=4
+w=5
+z=6
+a=9
+b=1
+d=(v+w)*z/a
+print("value of (v+w)*z/a=",a)
+name="Alex"
+Age=2
+if name=="Alex" or name=="John" and Age==0:
+    print("Hello!")
+else:
+    print("Goodbye!")
